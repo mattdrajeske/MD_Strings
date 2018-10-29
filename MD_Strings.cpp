5a70f396552bbb802b84d568812d129fa97227cb
Matt <drajeskematt@gmail.com>
2018-10-28 19:09:59 -0600

    add finished project

--- MD_Strings.cpp
+++ MD_Strings.cpp
@@ -16,10 +16,32 @@ int main()
 	string rand;
 
 	cout << "Enter your name:\n";
-	cin.get(name, name.length()-1);
-	cout << "\n" << name;
+	getline(cin, name);
+	cout << "\n";
 
-	//cout << 
+	cout << "Enter any number of random characters:\n";
+	getline(cin, rand);
+	cout << "\n";
 
+	reverse(rand);
+	putInMiddle(rand, name);
+
+	cout << "\n" << name << "\n\n";
+
+}
+
+void reverse(string& rand) {
+	string temp = rand;
+	for (int i = 0; i < rand.length()/2; i++) {
+		rand[i] = temp[rand.length() - i - 1];
+		rand[rand.length() - i - 1] = temp[i];
+	}
+}
+
+void putInMiddle(string reversedRand, string& name) {
+	string str1 = reversedRand.substr(0, reversedRand.length() / 2);
+	string str2 = reversedRand.substr(reversedRand.length() / 2, reversedRand.length() - 1);
+
+	name = str1 + " " + name + " " + str2;
 }
 
